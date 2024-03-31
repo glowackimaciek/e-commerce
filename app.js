e07387d1f6c02799c39bf38de2c49f79d8ce2691
Maciej <macglowacki@gmail.com>
2024-03-31 13:05:35 +0200

    add cryptoJS and hashed password

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import connectToDatabase from './src/config/connect.js';
 import 'dotenv/config';
+import registerRoute from './src/routes/register.js';
+import loginRoute from './src/routes/login.js';
 
 
-const app =express();
+const app = express();
 connectToDatabase();
 const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/register', registerRoute);
+app.use('/login', loginRoute);
 
 
 
